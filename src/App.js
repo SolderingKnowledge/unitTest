baae5f02ee1d1c4cfced35c0603384ef5e8c95e1
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-07-18 15:13:28 -0700

    Integrated react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Home from "./components/Home";
+class App extends Component {
+    render(){
+        return (
+            <div>
+                Hello world!
+                <Home />
+            </div>
+        )
+    }
+}
+
+export default App;
